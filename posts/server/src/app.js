0cd1e9a80f1b1684e7b83b53590f5cf8f18f1e8c
bambriz <bambriz@ucsc.edu>
2018-02-22 20:17:30 -0800

    Client server
    Adding a way to communicate between client and server

--- posts/server/src/app.js
+++ posts/server/src/app.js
@@ -1,1 +1,33 @@
-console.log('Hello World!');
+// This code belongs to Bryan Erick Ambriz
+// 02/22/2018
+
+
+//put all libraries into variables for use
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+// app stuff now
+//
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+//stuff
+
+app.get('/posts',(req, res) =>{
+	res.send(
+		[
+			{
+				title: "Hello World!",
+				description: "Hi there! How are you?"
+			}
+		]
+	)
+})
+
+//listener
+//
+app.listen(process.env.PORT || 8081
+)
